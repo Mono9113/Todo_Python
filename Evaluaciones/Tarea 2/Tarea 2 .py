b63ec868cd304a544da71cfe09983dd85e8d907c
MoNo9113 <tomas.monardez9113@gmail.com>
2025-06-02 20:23:59 -0400

    Update Tarea 2 .py

--- Evaluaciones/Tarea 2/Tarea 2 .py
+++ Evaluaciones/Tarea 2/Tarea 2 .py
@@ -8,8 +8,27 @@ class libro:
         self.edicion=edicion
         self.año=año
         self.costo=costo
-    def mostrar():
-        print()
 
-libro1= libro("123","caca","nada","leo dicaprio","pei","2","2050","2500")
-print(libro1)
+    def mostrar(self):
+        return f"""ISBN: {self.isbn}
+        Libro: {self.titulo} 
+Especialidad: {self.especialidad}
+Autor: {self.autor}
+Editorial: {self.editorial}
+Edicion: {self.edicion}
+Año: {self.año}
+Costo: {self.costo}"""
+
+
+
+#COSTO
+while True:
+    try:
+        costo = int(input("Ingrese el costo del libro (máximo $3000): "))
+        if costo <= 3000 and costo > 0:
+            break
+        else:
+            print("El costo debe ser un número mayor a 0 y menor o igual a $3000. Intente nuevamente.")
+    except ValueError:
+        print("Por favor, ingrese un valor numérico válido.")
+
