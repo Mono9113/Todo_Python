452cb08848445c72c68c1c275c10576bca13fe95
MoNo9113 <tomas.monardez9113@gmail.com>
2025-06-08 17:46:48 -0400

    08-06-2025

---
+++ Evaluaciones/Controles/Control 2.py/Control 3.py
@@ -0,0 +1,52 @@
+f=0
+class ventas():
+    def __init__(self,nombre_vendedor,precio_articulo,monto_comision):
+        global f
+        self.nombre_vendedor=str(nombre_vendedor)
+        self.precio_articulo=int(precio_articulo)
+        self.monto_comision=int(monto_comision*0)
+        f=1
+    def calcular_comision(self):
+        if self.precio_articulo < 30000:
+            self.monto_comision = self.precio_articulo*0.02
+        elif self.precio_articulo in range (30000,50000):
+            self.monto_comision = self.precio_articulo*0.04
+        elif self.precio_articulo >= 50000:
+            self.monto_comision = self.precio_articulo*0.05
+    def __str__(self):
+        return f"""
+\n---------------------
+Nombre vendedor: {self.nombre_vendedor}
+Precio articulo: {self.precio_articulo}
+Monto comision: {self.monto_comision}
+---------------------"""
+
+#Pp
+
+while True:
+    print("\n---Menu---")
+    print("1.Ingreso de datos y creacion del objeto.")
+    print("2.Calcular comision.")
+    print("3.Mostrar datos (estado) del objeto.")
+    print("4.Salir")
+    op=int(input("Ingrese opcion: "))
+    if op==1:
+        a=input("\nIngrese nombre vendedor: ")
+        b=int(input("Ingrese precio articulo: "))
+        c=0
+        comision=ventas(a,b,c)
+    if op==2:
+        if f==1:
+            print("\nSe ha calculado la comision.")
+            comision.calcular_comision()
+        elif f==0:
+            print("\nNo se a creado el articulo.\nEliga opcion 1.")
+    if op==3:
+        if f==1:
+            print(comision)
+        elif f==0:
+            print("\nNo se a creado el articulo.\nEliga opcion 1.")
+    if op==4:
+        print("\n<PROGRAMA TERMINADO>\n")
+        break
+
---
+++ Evaluaciones/Controles/Ejercicio control 3.py
@@ -0,0 +1,49 @@
+
+f=0
+class compra():
+    def __init__(self,nombre_cliente,monto_compra):
+        global f
+        self.nombre_cliente= str(nombre_cliente)
+        self.monto_compra=float(monto_compra)
+        f=1
+    def calcular_descuento(self):
+        if self.monto_compra <= 100:
+            self.monto_descuento=0
+        elif self.monto_compra in range (100,501):
+            self.monto_descuento=self.monto_compra*0.05
+        elif self.monto_compra >= 500:
+            self.monto_descuento=self.monto_compra*0.1 
+
+    def __str__(self):
+        return f"\n---------------\nNombre: {self.nombre_cliente} \nMonto sin descuento: {self.monto_compra} \nMonto con decuento: {self.monto_descuento}\n --------------------------"
+        
+
+#Programa
+
+
+while True: 
+    print("\n---MENU TIENDA---")
+    print("1. Ingresar datos y crear compra")
+    print("2. Calcular descuento")
+    print("3. Mostrar detalles dela compra")
+    print("4. Salir")
+    op=int(input("Ingrese opcion: "))
+
+    if op==1:
+        a=input("\nIngrese Nombre: ")
+        b=float(input("Ingrese Monto: "))
+        obj=compra(a,b)
+    if op==2:
+        if f==1:
+            print("\n Desceunto calculado.")
+            obj.calcular_descuento()
+        elif f==0:
+            print("\nNo se a creado una compra, eliga opcion 1.")
+    if op==3:
+        if f==1:
+            print(obj)
+        elif f==0:
+            print("\nNo se a creado una compra, eliga opcion 1.")
+    if op==4:
+        break
+    
