6deabd2f4c51c8807d5399c83739324be2fbbb47
Mono9113 <t.monardezhormazabal@uandresbello.edu>
2025-06-15 19:57:28 -0400

    15-06-2025

--- Evaluaciones/Tarea 2/Tarea 2 .py
+++ Evaluaciones/Tarea 2/Tarea 2 .py
@@ -1,4 +1,14 @@
-class libro:
+import matplotlib.pyplot as plt
+
+
+datos_libros = []
+datos_personas = []
+
+nombres = []
+costos = []
+
+
+class Libro:
     def __init__(self,isbn,titulo,especialidad,autor,editorial,edicion,año,costo):
         self.isbn=isbn
         self.titulo=titulo
@@ -12,23 +22,54 @@ class libro:
     def mostrar(self):
         return f"""ISBN: {self.isbn}
         Libro: {self.titulo} 
-Especialidad: {self.especialidad}
-Autor: {self.autor}
-Editorial: {self.editorial}
-Edicion: {self.edicion}
-Año: {self.año}
-Costo: {self.costo}"""
+        Especialidad: {self.especialidad}
+        Autor: {self.autor}
+        Editorial: {self.editorial}
+        Edicion: {self.edicion}
+        Año: {self.año}
+        Costo: {self.costo}"""
 
 class Inventario:
     def __init__(self):
-        self.productos = {}
+        global datos_libros
+        self.datos_libros = datos_libros
 
     def agregar_producto(self,isbn,titulo,especialidad,autor,editorial,edicion,año,costo ):
-        if titulo in self.productos:
+        if titulo in self.datos_libros:
             return f"El Libro {titulo},ISBN: {isbn} ya está en el inventario."
-        self.productos[titulo] = libro(isbn,titulo,especialidad,autor,editorial,edicion,año,costo )
+        datos_libros[titulo] = Libro(isbn,titulo,especialidad,autor,editorial,edicion,año,costo )
         return f"Libro {titulo} agregado al inventario."
 
+class Personas:
+    global datos_personas
+    global nombres
+    def __init__(self, rut, primer_nombre, segundo_nombre, apellido_paterno, apellido_materno, phono, email, nacimiento, direccion):
+        self.rut = rut
+        self.primer_nombre = primer_nombre
+        self.segundo_nombre = segundo_nombre
+        self.apellido_paterno = apellido_paterno
+        self.apellido_materno = apellido_materno
+        self.phono = phono
+        self.email = email
+        self.nacimiento = nacimiento
+        self.direccion = direccion
+        
+    def agregar_personas(self, rut, primer_nombre, segundo_nombre,apellido_paterno, apellido_materno, phono, email, nacimiento, direccion):
+        if primer_nombre in datos_personas:
+            return f"La persona {primer_nombre} {apellido_paterno} {apellido_materno},rut: {rut} ya está ingresada."
+        datos_personas[primer_nombre] = Personas(self, rut, primer_nombre, segundo_nombre, apellido_paterno, apellido_materno, phono, email, nacimiento, direccion)
+        return f"Usuario {primer_nombre} ingresado exitosamente."
+        
+
+    def __str__(self):
+        return f"""Nombres y apellidos: {self.primer_nombre} {self.segundo_nombre} {self.apellido_paterno} {self.apellido_materno}
+        Rut: {self.rut}
+        Nacimiento: {self.nacimiento}
+        Email: {self.email}
+        Phono: {self.phono}
+        Direccion: {self.direccion}
+        """
+    
 #MENU
 def mostrar_menu():
     print("\n--- SISTEMA DE PRÉSTAMO DE LIBROS ---")
@@ -41,36 +82,72 @@ def mostrar_menu():
     return input("Selecciona una opción: ")
 
 def main():
-    inventario = Inventario()
     while True:
         opcion = mostrar_menu()
         if opcion == '1': #Ingresar daros libros prestados
-            isbn = int(input("ISBN: "))
-            titulo = input("Titulo: ")
-            especialidad = input("Especialidad: ")
-            autor= input("Autor: ")
-            editorial = input("Editorial: ")
-            edicion = input("Edicion: ")
-            año = int(input("Año: "))
-            #Costo
+            datos_libros_ingresados = True
+            isbn = str(input("Ingrese ISBN: "))
+            titulo = str(input("Ingrese título del libro: "))
+            especialidad = str(input("Ingrese especialidad: "))
+            autor = str(input("Ingrese el nombre del autor: "))
+            editorial = str(input("Ingrese editorial: "))
+            edicion = str(input("Ingrese edición del libro: "))
+            año = str(input("Ingrese año del libro: "))
             while True:
                 try:
                     costo = int(input("Ingrese el costo del libro (máximo $3000): "))
                     if costo <= 3000 and costo > 0:
+                        costo=costo
                         break
                     else:
                         print("El costo debe ser un número mayor a 0 y menor o igual a $3000. Intente nuevamente.")
                 except ValueError:
                     print("Por favor, ingrese un valor numérico válido.")
-            print(inventario.agregar_producto(isbn,titulo,especialidad,autor,editorial,edicion,año,costo))
+            libro = Libro(isbn, titulo, especialidad, autor, editorial, edicion, año, costo)
+            datos_libros.append(libro)
+            costos.append(costo)
+            Inventario.agregar_producto(isbn,titulo,especialidad,autor,editorial,edicion,año,costo)
+
         elif opcion == '2': #Ingresar datos personas prestan
-            print()
+            datos_personas_ingresados=True
+            print("opcion 2")
+            rut=str(input("ingrese rut:"))
+            primer_nombre=str(input("ingrese primer nombre: "))
+            segundo_nombre=str(input("ingrese segundo nombre: "))
+            apellido_paterno=str(input("ingrese apellido paterno: "))
+            apellido_materno=str(input("ingrese apellido materno: "))
+            phono=str(input("ingrese numero de telefono: "))
+            email=str(input("ingrese correo electronico: "))
+            nacimiento=str(input("ingrese fecha de nacimiento dd/mm/aa: "))
+            direccion=str(input("ingrese su dirección: "))
+            print(f"Costo: {costo}")
+            people = Personas(rut, primer_nombre, segundo_nombre, apellido_paterno, apellido_materno, phono, email, nacimiento, direccion)
+            nombres.append(primer_nombre)
+
         elif opcion == '3': #V. datos libros prestados
-            print()
+            if datos_libros_ingresados == True:
+                for i in range(0,len(datos_libros)):
+                    libro.mostrar()
+            else:
+                print("No hay datos ingresados")
+
         elif opcion == '4': #V. datos personas prestan
-            print()
+            if datos_personas_ingresados == True:
+                print("opcion 4")
+                for i in range(0,len(datos_personas)):
+                    people.__str__()
+            else:
+                print("No hay datos ingresados")
+
         elif opcion == '5': #Grafico
-            print()
+            print("opcion 5")
+            if datos_libros_ingresados == True and datos_personas_ingresados == True:
+                plt.bar(nombres, costos, color="skyblue")
+                plt.title("Grárfico de barras")
+                plt.xlabel("Personas")
+                plt.ylabel("Monto a pagar")
+                plt.show()
+
         elif opcion == '6': #Salir
             print("¡Gracias por usar el sistema de inventario!")
             break
